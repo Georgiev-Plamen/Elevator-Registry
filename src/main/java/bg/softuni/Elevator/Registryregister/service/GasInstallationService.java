b0a66f61e7203e7565357a741028032d4c233b6c
Plamen Georgiev <plamen_gg@mail.bg>
2024-07-20 06:57:46 +0300

    First step of restAPI

---
+++ src/main/java/bg/softuni/Elevator/Registryregister/service/GasInstallationService.java
@@ -0,0 +1,7 @@
+package bg.softuni.Elevator.Registryregister.service;
+
+import bg.softuni.Elevator.Registryregister.model.dto.GasInstallationDTOs.AddGasInstallationDTO;
+
+public interface GasInstallationService {
+    void addGasInstallation(AddGasInstallationDTO addGasInstallationDTO);
+}
---
+++ src/main/java/bg/softuni/Elevator/Registryregister/service/impl/GasInstallationServiceImpl.java
@@ -0,0 +1,22 @@
+//package bg.softuni.Elevator.Registryregister.service.impl;
+//
+//import bg.softuni.Elevator.Registryregister.model.dto.GasInstallationDTOs.AddGasInstallationDTO;
+//import bg.softuni.Elevator.Registryregister.service.GasInstallationService;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestClient;
+//
+//@Service
+//public class GasInstallationServiceImpl implements GasInstallationService {
+//
+//    private final RestClient gasInstallationClient;
+//
+//    public GasInstallationServiceImpl(RestClient gasInstallationClicent) {
+//        this.gasInstallationClient = gasInstallationClicent;
+//    }
+//
+//    @Override
+//    public void addGasInstallation(AddGasInstallationDTO addGasInstallationDTO) {
+//
+//
+//    }
+//}
